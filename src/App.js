4899f199e1f182b49f0fadeea1f0b60b81b71b98
git-Sjd <sajid.tisl@gmail.com>
2023-12-13 02:02:36 +0530

    E-commerce-App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Container from './components/Container';
+import Product from './components/Product';
 
-import HomePage from './components/HomePage';
+
 
 function App() {
+
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Header />
+      {/* <Wallpaper /> */}
+      <Routes>
+        <Route exact path='/' Component={Container} />
+        <Route path='/Product/:id/:name' Component={Product} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function About() {
-    return (
-        <div className='bg-blue-500 text-4xl mx-auto'>
-            This is the about section
-        </div>
-    )
-}
